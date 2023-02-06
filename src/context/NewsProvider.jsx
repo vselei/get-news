fe7cc485834915b2f67e06f05dc18c4f61efe54c
Victor Seleimend <vsennaselei@gmail.com>
2023-02-06 11:34:46 -0300

    api request

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -1,9 +1,22 @@
 import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 
 const NewsContext = createContext();
 
 const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState('general');
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const requestAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=br&category=${category}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const { data: articles } = await axios(url);
+      setNews(articles);
+    };
+    requestAPI();
+  }, [category]);
 
   const handleCategoryChange = e => setCategory(e.target.value);
 
